ea784d764129832443853012297b82161e948603
StoryChara <mjarah@unal.edu.co>
2026-01-07 13:24:35 -0500

    add skeletons and modal for cv

--- src/util/CVModal.js
+++ src/util/CVModal.js
@@ -4,7 +4,7 @@ import './CVModal.css';
 
 const CVModal = ({ show, handleClose }) => {
   const [loading, setLoading] = useState(true);
-  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const handleLoad = () => {
     setLoading(false);
